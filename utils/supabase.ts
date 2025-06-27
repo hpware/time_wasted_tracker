ed37e9621317ac27dfa2b5c93d9a329caf2e13aa
吳元皓 <hw@yuanhau.com>
2025-06-27 10:39:16 +0800

    Fix some stuff.

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://sxzsbiunmyqmxaldtjfp.supabase.co";
-const supabaseAnonKey = process.env.EXPO_SUPABASE_ANON | ""; // I don't want to hard code it, since we are publishing to github.
+const supabaseAnonKey = process.env.EXPO_SUPABASE_ANON || ""; // I don't want to hard code it, since we are publishing to github.
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
