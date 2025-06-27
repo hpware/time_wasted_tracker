0ac75d4e6abde7951f72d8a8d9a516b8ebdd22b3
吳元皓 <hw@yuanhau.com>
2025-06-27 10:01:20 +0800

    Get supabase.

---
+++ utils/supabase.ts
@@ -0,0 +1,15 @@
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://sxzsbiunmyqmxaldtjfp.supabase.co";
+const supabaseAnonKey = process.env.EXPO_SUPABASE_ANON | ""; // I don't want to hard code it, since we are publishing to github.
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
