4c09939fdef409a3e9b555813b793cc193745c37
吳元皓 <hw@yuanhau.com>
2025-06-29 05:45:22 +0800

    Spinner & Progress bar.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
-import { Progress } from "@/components/ui/progress";
+import * as Progress from "react-native-progress";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Progress />
+      <Progress.Bar progress={0.9} width={200} />
+      <Progress.Pie progress={0.4} size={50} />
+      <Progress.Circle size={30} indeterminate={true} />
+      <Progress.CircleSnail color={["red", "green", "blue"]} />
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
