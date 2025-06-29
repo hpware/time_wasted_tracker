911d3083b546b1a635cdc0398cd2b3e0b1042f01
吳元皓 <hw@yuanhau.com>
2025-06-30 00:15:28 +0800

    Add some basic logic into the app. (I still like javascript compared to Swift), I don't really want to touch swiftn ever again.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,37 @@
-import { View, Text } from "react-native";
-import * as Progress from "react-native-progress";
+import { View, Text, Pressable } from "react-native";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [timerStarted, setTimerStarted] = useState<boolean>(false);
+  const startTimer = () => {
+    setTimerStarted(true);
+  };
+  const endTimer = () => {
+    setTimerStarted(false);
+  };
   return (
     <View className="items-center justify-center mt-12 flex flex-col">
-      <View className="flex flex-col items-center justify-center align-middle m-2">
-        <Text className="text-2xl dark:text-white m-3">
-          Time wasted on <Text className="font-bitcount">{"something"}</Text>
-        </Text>
-        <Progress.Circle
-          size={200}
-          progress={0.1}
-          thickness={30}
-          showsText={true}
-          formatText={() => `${10}%`}
-        />
-      </View>
+      <Text className="text-2xl dark:text-white m-3">
+        Time wasted on <Text className="font-bitcount">{"something"}</Text>
+      </Text>
+      {!timerStarted ? (
+        <Pressable
+          className="rounded bg-sky-600/60 text-white p-2 px-5 text-lg"
+          onPress={startTimer}
+        >
+          <Text className="text-white text-lg">Start</Text>
+        </Pressable>
+      ) : (
+        <View>
+          <Text className="text-lg dark:text-white m-2">Timer: {"11:40"}</Text>
+          <Pressable
+            className="rounded bg-sky-600/60 text-white p-2 px-5 text-lg"
+            onPress={endTimer}
+          >
+            <Text className="text-white text-lg">End timer</Text>
+          </Pressable>
+        </View>
+      )}
     </View>
   );
 }
